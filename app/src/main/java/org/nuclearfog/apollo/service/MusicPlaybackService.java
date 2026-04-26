9a35acdb11e10f3b3989bf0fa7cc65caa8edff0d
nuclearfog <nuclearfog@noreply.codeberg.org>
2026-04-26 23:42:51 +0200

    fixed crash/media button handling

--- app/src/main/java/org/nuclearfog/apollo/service/MusicPlaybackService.java
+++ app/src/main/java/org/nuclearfog/apollo/service/MusicPlaybackService.java
@@ -911,7 +911,7 @@ public class MusicPlaybackService extends Service implements OnAudioFocusChangeL
 	private void updatePlaybackState() {
 		PlaybackStateCompat.Builder builder = new PlaybackStateCompat.Builder();
 		builder.setState(mPlayer.isPlaying() ? PlaybackStateCompat.STATE_PLAYING : PlaybackStateCompat.STATE_PAUSED, mPlayer.getPosition(), 1.0f);
-		builder.setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE | PlaybackStateCompat.ACTION_STOP |
+		builder.setActions(PlaybackStateCompat.ACTION_PLAY | PlaybackStateCompat.ACTION_PAUSE | PlaybackStateCompat.ACTION_STOP |
 				PlaybackStateCompat.ACTION_SKIP_TO_NEXT | PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS |
 				PlaybackStateCompat.ACTION_PLAY_FROM_URI | PlaybackStateCompat.ACTION_SEEK_TO |
 				PlaybackStateCompat.ACTION_SET_SHUFFLE_MODE | PlaybackStateCompat.ACTION_SET_REPEAT_MODE);
